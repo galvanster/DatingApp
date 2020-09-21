23f5102cde084e1fc569926e9a689bd29c628f00
galvanster@gmail.com <galvanster@gmail.com>
2020-09-20 22:47:47 -0500

    section 128

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +12,35 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister =  new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authservice: AuthService, private alertify: AlertifyService) { }
+  constructor(private authservice: AuthService, 
+    private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true };
+  }
+
+
   register() {
-    this.authservice.register(this.model).subscribe(() => {
-      this.alertify.success('registration successful');
-    }, error => {
-        this.alertify.error(error);
-    });
+   // this.authservice.register(this.model).subscribe(() => {
+   //   this.alertify.success('registration successful');
+   // }, error => {
+   //     this.alertify.error(error);
+   // });
+   console.log(this.registerForm.value);
   }
 
   cancel(){
